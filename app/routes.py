3e2db6cad20213b7f154bbc17d27d4eb74b57ee9
sridharansuriya <asterixsurya@gmail.com>
2021-01-21 16:27:41 +0530

    v3.0 commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +14,12 @@ def index():
         'body': 'All credits to me'
     }]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login request received for user {form.username.data} and remember_me {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
